724ac5c3aaf499e93f6410e07344960c0c9d6438
Mohammed Aloshaibi <mohammed.aloshaiby@gmail.com>
2024-01-23 17:11:53 +0300

    first commit

---
+++ delegate ex/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+
+
+
+        //printDelegate pr= new printDelegate(class1.printMassage);
+        //pr("mohammed");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+//public delegate void printDelegate(string Massage);
+//public class class1
+//{
+//    public static void printMassage(string Massage)
+//    {
+//        Console.WriteLine(Massage);
+//    }
+//}
